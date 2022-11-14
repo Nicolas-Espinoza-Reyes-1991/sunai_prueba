86253ddde854cbe562507f28bf44d5bec7d3fcd0
Nicolas Espinoza <nicolas.espinoza@senda.gob.cl>
2022-11-14 19:55:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+// import FormSunai from './components/FormSunai';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup ,ModalFooter} from 'reactstrap';
+import FormSunai from "./components/FormSunai";
+import GaraficoExcel from "./components/GaraficoExcel";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  render() {
+    return (
+
+      <div>
+      <FormSunai></FormSunai>
+
+      <GaraficoExcel></GaraficoExcel>
+  </div>
+      
+    )}
+    
+      
 }
+
+
 
 export default App;
